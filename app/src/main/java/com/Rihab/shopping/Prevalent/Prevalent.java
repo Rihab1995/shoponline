cc822158677772de2a667354730452a757d6f702
Rihab1995 <rhbabsy@gmail.com>
2021-12-15 13:17:15 +0100

    Adding my files

---
+++ app/src/main/java/com/Rihab/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.Rihab.shopping.Prevalent;
+
+import com.Rihab.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
